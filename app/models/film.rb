05a505bb1a83e3dc0a4f2048a260759008edd49c
Piotr Lac <piotr.lac@protonmail.ch>
2018-01-07 12:41:11 +0100

    add validation

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,2 +1,4 @@
 class Film < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 3, maximum: 50}
+  validates :description, presence: true, length: { minimum: 3, maximum: 50}
 end
