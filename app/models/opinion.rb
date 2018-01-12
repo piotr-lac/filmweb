a52006382a559af68bbdf400a64cfdd78b9cc962
Piotr Lac <piotr.lac@protonmail.ch>
2018-01-12 18:57:54 +0100

    add validation to ranking 1-5

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,7 +1,7 @@
 class Opinion < ApplicationRecord
   validates :nick, presence: true, length: { minimum: 3, maximum: 50}
   validates :review, presence: true, length: { minimum: 3, maximum: 50}
-  validates :rating, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }
 
   belongs_to :film
 end
